9b119d12e19b666ed31ca1927294c67a94c262ef
Timotius Jonathan <timotius.jonathan95@gmail.com>
2025-12-19 14:12:32 +0700

    feat: add logger middleware and start Gin server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,16 +3,35 @@ package main
 import (
 	"log"
 
+	"github.com/TimX-21/auth-service-go/internal/config"
+	"github.com/TimX-21/auth-service-go/internal/middlewares"
 	"github.com/TimX-21/auth-service-go/pkg"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
+
+	err := config.InitZapSugaredLogger()
+	if err != nil {
+		log.Fatalf("Failed to initialize logger: %v", err)
+		return
+	}
+	
+	defer config.Log.Sync()
+
 	db, err := pkg.ConnectDB()
 	if err != nil {
 		log.Fatalf("DB connection failed: %v", err)
+		return
 	}
 
-	log.Println("DB connected successfully")
+	router := gin.New()
+
+	router.Use(gin.Recovery())
+	router.ContextWithFallback = true
+	router.Use(middlewares.LoggerMiddleware())
+
+	router.Run(":8080")
 
 	_ = db.Close()
 }
